55aea8eeac54dcf182d2f390832531a8989a89de
Simon Peck <simon@belscar.com>
2025-07-31 14:47:56 +0100

    Update environment variable usage and Firebase integration

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,7 +1,6 @@
 import { getApps, initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { PrismaClient } from "@prisma/client";
-import { readFileSync } from "fs";
 
 declare global {
   var prismaGlobal: PrismaClient;
@@ -18,9 +17,13 @@ const prisma = global.prismaGlobal ?? new PrismaClient();
 let firestore: FirebaseFirestore.Firestore;
 
 if (!getApps().length) {
-  const serviceAccount = JSON.parse(
-    readFileSync("./serviceAccountKey.json", "utf8")
-  );
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : undefined,
+  };
 
   initializeApp({
     credential: cert(serviceAccount),
